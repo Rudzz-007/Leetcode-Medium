77145d8e3aa47a18184a2b60c4fb0a5c17774d9b
Rudzz-007 <rudragrana17@gmail.com>
2026-02-06 07:10:06 +0530

    Leetcode 861

---
+++ Score After Flipping Matrix/Score_After_Flipping_Matrix.cpp
@@ -0,0 +1,65 @@
+class Solution {
+public:
+    int matrixScore(vector<vector<int>>& grid) {
+        int rows = grid.size();
+        int cols = grid[0].size();
+
+        // making the first column all 1's
+        for (int i = 0; i < rows; i++) {
+            if (grid[i][0] == 0) { // flip
+                for (int j = 0; j < cols; j++) {
+                    if (grid[i][j] == 0)
+                        grid[i][j] = 1;
+                    else
+                        grid[i][j] = 0;
+                }
+            }
+        }
+
+        // flip the columns where no > zno
+        for (int j = 0; j < cols; j++) {
+            int noz = 0;
+            int zno = 0;
+            for (int i = 0; i < rows; i++) {
+                if (grid[i][j] == 0)
+                    noz++;
+                else
+                    zno++;
+            }
+
+            if (noz > zno) { // flip
+                for (int i = 0; i < rows; i++) {
+                    if (grid[i][j] == 0)
+                        grid[i][j] = 1;
+                    else
+                        grid[i][j] = 0;
+                }
+            }
+        }
+        int noz = 0;
+        int noo = 0;
+        for (int i = 0; i < rows; i++) {
+            if (grid[i][0] == 0)
+                noz++;
+            else
+                noo++;
+        }
+        if (noz > noo) { // flip
+            for (int i = 0; i < rows; i++) {
+                if (grid[i][0] == 0)
+                    grid[i][0] = 1;
+                else
+                    grid[i][0] = 0;
+            }
+        }
+        int sum = 0;
+        for (int i = 0; i < rows; i++) {
+            int x = 1;
+            for (int j = cols - 1; j >= 0; j--) {
+                sum += grid[i][j] * x;
+                x *= 2;
+            }
+        }
+        return sum;
+    }
+};
